ce5e9b60fc6100447063919cb00285f2182f232d
Vikash Batham <31383789+Vikash7TM@users.noreply.github.com>
2020-10-23 16:17:57 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -6,64 +6,70 @@ import java.util.Scanner;
 //no. of rows->arr_name.length
 //no. of columns->arr_name[0].length
 public class Main {
-    public static Scanner scn = new Scanner(System.in);
+	public static Scanner scn = new Scanner(System.in);
     public static void main(String[] args) {
         while (true) {
             switch (choiceMenu()){
-                case 1:{int m1row = scn.nextInt();
+                case 1:{
+                	System.out.println("Enter the row and column size for matrix 1 ::");
+                	int m1row = scn.nextInt();
                     int m1col = scn.nextInt();
-                    double[][] mtx1 = new double[m1row][m1col];
+                    int[][] mtx1 = new int[m1row][m1col];
                     setMatrix(mtx1);
-
+                    System.out.println("Enter the row and column size for matrix 2 ::");
                     int m2row = scn.nextInt();
                     int m2col = scn.nextInt();
-                    double[][] mtx2 = new double[m2row][m2col];
+                    int[][] mtx2 = new int[m2row][m2col];
                     setMatrix(mtx2);
                     if(m1row==m2row && m1col==m2col){
-                        double[][] add_mtx=new double[m1row][m1col];
+                        int[][] add_mtx=new int[m1row][m1col];
                         addingMatrix(mtx1,mtx2,add_mtx);
                         displayMatrix(add_mtx);
                     }else{
-                        System.out.println("ERROR");
+                        System.out.println("ERROR row and column size not matching");
                     }} break;
-                case 2:{int m1row = scn.nextInt();
+                case 2:{
+                	System.out.println("Enter the row and column size ::");
+                	int m1row = scn.nextInt();
                     int m1col = scn.nextInt();
-                    double[][] mtx1 = new double[m1row][m1col];
+                    int[][] mtx1 = new int[m1row][m1col];
                     setMatrix(mtx1);
+                    System.out.println("Enter the muliplication constant ::");
                     int num = scn.nextInt();
+                    displayMatrix(mtx1);
                     multiplicationNum(mtx1,num);
+                    System.out.println("After multiplication :: ");
                     displayMatrix(mtx1);} break;
-                case 3:{int m1row = scn.nextInt();
+                case 3:{
+                	System.out.println("Enter the row and column size for matrix 1::");
+                	int m1row = scn.nextInt();
                     int m1col = scn.nextInt();
-                    double[][] mtx1 = new double[m1row][m1col];
+                    int[][] mtx1 = new int[m1row][m1col];
                     setMatrix(mtx1);
-
+                    
+                    System.out.println("Enter the row and column size for matrix 2::");
                     int m2row = scn.nextInt();
                     int m2col = scn.nextInt();
-                    double[][] mtx2 = new double[m2row][m2col];
+                    int[][] mtx2 = new int[m2row][m2col];
                     setMatrix(mtx2);
                     if(m1col==m2row){
-                        double[][] mlt_mtx=new double[m1row][m2col];
+                        int[][] mlt_mtx=new int[m1row][m2col];
                         multiplyMatrix(mtx1,mtx2,mlt_mtx);
                         displayMatrix(mlt_mtx);
                     }else{
                         System.out.println("ERROR");
                     }} break;
                 case 4:{
-                    System.out.println("1. Main diagonal\n" +
-                            "2. Side diagonal\n" +
-                            "3. Vertical line\n" +
-                            "4. Horizontal line");
-                    int transposeChoice = scn.nextInt();
+                	System.out.println("Enter the row and column size ::");
                     int mrow = scn.nextInt();
                     int mcol = scn.nextInt();
-                    double[][] mtx = new double[mrow][mcol];
+                    int[][] mtx = new int[mrow][mcol];
+                    int[][] tmtx = new int[mcol][mrow];
                     setMatrix(mtx);
-                    if(transposeChoice == 1){mainDiagonalTranspose(mtx);}
-                    else if(transposeChoice == 2){sideDiagonalTranspose(mtx);}
-                    else if(transposeChoice == 3){verticalLineTranspose(mtx);}
-                    else if(transposeChoice == 4){horizontalLineTranspose(mtx);}
                     displayMatrix(mtx);
+                    mainDiagonalTranspose(mtx,tmtx);
+                    System.out.println("After transpose :: ");
+                    displayMatrix(tmtx);
                 } break;
                 case 0:System.exit(1); break;
 
@@ -71,39 +77,43 @@ public class Main {
         }
 
     }
-    public static void setMatrix(double[][] m){
-        for(int i=0;i<m.length;i++){
+    public static void setMatrix(int[][] m){
+        System.out.println("Enter data for matrix ::");
+    	for(int i=0;i<m.length;i++){
+    		System.out.println("Row "+(i+1));
             for(int j=0;j<m[0].length;j++){
-                m[i][j]=scn.nextDouble();
+            	System.out.println("Enter data for column "+(j+1));
+                m[i][j]=scn.nextInt();
             }
         }
     }
-    public static void displayMatrix(double[][] m){
-        for(int i=0;i<m.length;i++){
-            for(int j=0;j<m[0].length;j++){
-                System.out.print(m[i][j]);
-                System.out.print(" ");
-            }
-            System.out.println();
-        }
+    public static void displayMatrix(int[][] m){
+    	int i, j;
+		for (i = 0; i < m.length; i++) {
+			System.out.print(" [");
+			for (j = 0; j < m[i].length; j++) {
+				System.out.printf("%5d",m[i][j]);
+			}
+			System.out.println(" ]");
+		}
     }
-    public static void addingMatrix(double[][] m1,double[][] m2,double[][] nm){
+    public static void addingMatrix(int[][] m1,int[][] m2,int[][] nm){
         for(int i=0;i<m1.length;i++){
             for(int j=0;j<m1[0].length;j++){
                 nm[i][j]=m1[i][j]+m2[i][j];
             }
         }
     }
-    public static void multiplicationNum(double[][] m,int num){
+    public static void multiplicationNum(int[][] m,int num){
         for(int i=0;i<m.length;i++){
             for(int j=0;j<m[0].length;j++){
                 m[i][j]=num*m[i][j];
             }
         }
     }
-    public static void multiplyMatrix(double[][] m1,double[][] m2,double[][] nm){
-        double temp=0;
-        double elem=0;
+    public static void multiplyMatrix(int[][] m1,int[][] m2,int[][] nm){
+        int temp=0;
+        int elem=0;
         for(int i=0;i<m1.length;i++){  //new matrix row count
             for(int j=0;j<m2[0].length;j++){  // new matrix column count
                 for(int k=0;k<m1[0].length;k++){ // count upto m1 column or m2 row count
@@ -115,59 +125,17 @@ public class Main {
             }
         }
     }
-    public static void mainDiagonalTranspose(double[][]m){
-        double temp=0;
-        for(int i=0;i<m.length-1;i++){
-            for(int j=1+i;j<m[0].length;j++){
-                temp = m[i][j];
-                m[i][j]=m[j][i];
-                m[j][i]=temp;
-            }
-        }
-    }
-    public static void sideDiagonalTranspose(double[][]m){
-        double temp=0;
-        int col2=m[0].length-1;
-        int col1;
-        for(int i=0;i<m.length-1;i++){
-            col1=m[0].length-1;
-            for(int j=0;j<m[0].length-1-i;j++){
-                temp=m[i][j];
-                m[i][j]=m[col1][col2];
-                m[col1][col2]=temp;
-                col1--;
-            }
-            col2--;
-        }
-    }
-    public static void verticalLineTranspose(double[][]m){
-        double temp=0;
-        int col;
-        for(int i=0;i<m.length;i++){
-            col = m[0].length-1;
-            for(int j=0;j<m[0].length/2;j++){
-               temp = m[i][j];
-               m[i][j]=m[i][col];
-               m[i][col]=temp;
-               col--;
-            }
-        }
-    }
-    public static void  horizontalLineTranspose(double[][]m){
-        double temp=0;
-        int row;
-        for(int j=0;j<m[0].length;j++){
-            row = m.length-1;
-            for(int i=0;i<m.length/2;i++){
-                temp = m[i][j];
-                m[i][j]=m[row][j];
-                m[row][j]=temp;
-                row--;
+
+	public static void mainDiagonalTranspose(int[][]m, int[][]t){
+        for(int i=0;i<t.length;i++){
+            for(int j=0;j<t[i].length;j++){
+            	t[i][j] = m[j][i];        
             }
         }
     }
 
     public static int choiceMenu(){
+    	System.out.println("");
         System.out.println("1. Add matrices\n" +
                 "2. Multiply matrix to a constant\n" +
                 "3. Multiply matrices\n" +
